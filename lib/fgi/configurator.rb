33f33b1f0cc40c85bda8657aafc85a784bea9d49
MatthieuGC <matthieu.gourvenec@gmail.com>
2017-05-18 17:04:48 +0200

    Feature/do not use tmp desc file anymore and added fun interrupt message (#8)
    * refacto configurator.rb and fixed some doubles quotes
    
    * updated Gemfile.lock
    
    * changed the README with the correct git urml
    
    * changed the way to create issues description and added fun messages when interrupt fgi
    
    * resolved conflicts
    
    * fixed conflicts
    
    * added cool messages when exiting fgi
    
    * added helper to quit/exit fgi

--- lib/fgi/configurator.rb
+++ lib/fgi/configurator.rb
@@ -4,12 +4,11 @@ module Fgi
 
       def run
         @config = Fgi::Config.current
-        not_git_directory = !File.exist?(File.expand_path('.gitignore'))
-        puts %q(This doesn't seem to be the root of a git repository, browse to the root of your project and try again.) if not_git_directory
-        return if not_git_directory
+        is_git_dir?
         puts '####################################################################'
         puts '            Welcome to Fast Gitlab Issues configuration             '
         puts "####################################################################\n\n"
+        puts "#### Enter 'quit' or 'exit' at any time to go back to buisness! ####\n\n"
 
         puts 'Please enter your Gitlab Url:'
         validate_and_save_gitlab_uri
@@ -29,25 +28,6 @@ module Fgi
         puts "\n####################################################################"
       end
 
-      def validate_and_save_gitlab_uri
-        puts 'example: http://gitlab.example.com/'
-        puts '-----------------------------------'
-        begin
-          input = "#{STDIN.gets.chomp}"
-          exit! if input == 'quit'
-          input = "http://#{input}" if !input.start_with?('http://', 'https://')
-          @uri = URI.parse("#{input}/")
-          @config[:url] = "#{@uri.scheme}://#{@uri.host}"
-          req = Net::HTTP.new(@uri.host, @uri.port)
-          res = req.request_head(@uri.path)
-        rescue Interrupt => int
-          exit!
-        rescue Exception => e
-          puts "\nOops, seems to be a bad url. Try again or quit (quit) :"
-          validate_and_save_gitlab_uri
-        end
-      end
-
       def validate_and_save_gitlab_token(inline_token = nil)
         begin
           @token = if inline_token.nil?
@@ -56,8 +36,12 @@ module Fgi
                      set_config
                      inline_token
                    end
-          exit! if @token == 'quit'
+          if %w(quit exit).include?(@token)
+            puts 'See you back soon !'
+            exit!
+          end
         rescue Interrupt => int
+          puts %q[Why did you killed me ? :'(]
           exit!
         end
         @projects_uri = "#{@config[:url]}/api/v4/projects"
@@ -69,9 +53,34 @@ module Fgi
         if res.code == '200'
           save_gitlab_token
         else
-          puts "\nOops, seems to be an invalid token. Try again or quit (quit) :"
+          puts "\nOops, seems to be an invalid token. Try again or quit (quit/exit) :"
           puts '--------------------------------------------------------------'
           validate_and_save_gitlab_token
+        end
+      end
+
+      private
+
+      def validate_and_save_gitlab_uri
+        puts 'example: http://gitlab.example.com/'
+        puts '-----------------------------------'
+        begin
+          input = STDIN.gets.chomp
+          if %w(quit exit).include?(input)
+            puts 'See you back soon !'
+            exit!
+          end
+          input = "http://#{input}" if !input.start_with?('http://', 'https://')
+          @uri = URI.parse("#{input}/")
+          @config[:url] = "#{@uri.scheme}://#{@uri.host}"
+          req = Net::HTTP.new(@uri.host, @uri.port)
+          res = req.request_head(@uri.path)
+        rescue Interrupt => int
+          puts %q[Why did you killed me ? :'(]
+          exit!
+        rescue Exception => e
+          puts "\nOops, seems to be a bad url. Try again or quit (quit/exit) :"
+          validate_and_save_gitlab_uri
         end
       end
 
@@ -90,8 +99,12 @@ module Fgi
       def search_and_save_project
         begin
           project_name = STDIN.gets.chomp
-          exit! if project_name == 'quit'
+          if %w(quit exit).include?(project_name)
+            puts 'See you back soon !'
+            exit!
+          end
         rescue Interrupt => int
+          puts %q[Why did you killed me ? :'(]
           exit!
         end
 
@@ -107,7 +120,7 @@ module Fgi
           end
           validate_option(results)
         else
-          puts "\nOops, we couldn't find a project called #{project_name}. Try again or quit (quit) :"
+          puts "\nOops, we couldn't find a project called #{project_name}. Try again or quit (quit/exit) :"
           puts '-------------------------------------------------------------------'+('-'*project_name.length) # Yes, i'm a perfectionist <3
           search_and_save_project
         end
@@ -119,6 +132,7 @@ module Fgi
         begin
           option = STDIN.gets.chomp.to_i
         rescue Interrupt => int
+          puts %q[Why did you killed me ? :'(]
           exit!
         end
         if (1..results.length+1).include?(option)
@@ -134,6 +148,14 @@ module Fgi
         config_file = File.expand_path(CONFIG_FILE)
         @config = YAML.load_file(config_file)
         @uri = URI.parse(@config[:url])
+      end
+
+      def is_git_dir?
+        is_git_directory = Dir.exists?('.git')
+        if !is_git_directory
+          puts %q(This doesn't seem to be the root of a git repository, browse to the root of your project and try again.)
+          return
+        end
       end
     end
   end
